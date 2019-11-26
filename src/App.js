85ddf4da93636d8f33ea438ebb94b68afeb48843
Phran <FrancisOBatson@gmail.com>
2019-11-26 15:30:23 -0500

    hoisted states and  mainsails

--- src/App.js
+++ src/App.js
@@ -4,22 +4,48 @@ import StatBlock from "./StatBlock";
 
 class App extends React.Component {
 
-  statchange = (name) => {
-    console.log("A stat has changed in " + name);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      str : {base: 10, modifier: 0},
+      dex : {base: 10, modifier: 0},
+      con : {base: 10, modifier: 0},
+      int : {base: 10, modifier: 0},
+      wis : {base: 10, modifier: 0},
+      cha : {base: 10, modifier: 0},
+    }
+}
+
+  statchange = (name, base, modifier) => {
+    switch(name) {
+      case "str" : this.setState({str: {base: base, modifier: modifier}})
+        break
+      case "dex" : this.setState({dex: {base: base, modifier: modifier}})
+        break
+      case "con" : this.setState({con: {base: base, modifier: modifier}})
+        break
+      case "int" : this.setState({int: {base: base, modifier: modifier}})
+        break
+      case "wis" : this.setState({wis: {base: base, modifier: modifier}})
+        break
+      case "cha" : this.setState({cha: {base: base, modifier: modifier}})
+        break
+    }
   }
   
-  render(){
+  render(i){
     return (
       <div className="App">
         <h1>Character Sheet</h1>
         <div className="name"><textarea></textarea></div>
         <div className="statWrapper">
-          <StatBlock name="str" statchange={this.statchange} />
-          <StatBlock name="dex" statchange={this.statchange} />
-          <StatBlock name="con" statchange={this.statchange} />
-          <StatBlock name="int" statchange={this.statchange} />
-          <StatBlock name="wis" statchange={this.statchange} />
-          <StatBlock name="cha" statchange={this.statchange} />
+          <StatBlock name="str" statchange={this.statchange} base={this.state.str.base} modifier={this.state.str.modifier} />
+          <StatBlock name="dex" statchange={this.statchange} base={this.state.dex.base} modifier={this.state.dex.modifier}/>
+          <StatBlock name="con" statchange={this.statchange} base={this.state.con.base} modifier={this.state.con.modifier}/>
+          <StatBlock name="int" statchange={this.statchange} base={this.state.int.base} modifier={this.state.int.modifier}/>
+          <StatBlock name="wis" statchange={this.statchange} base={this.state.wis.base} modifier={this.state.wis.modifier}/>
+          <StatBlock name="cha" statchange={this.statchange} base={this.state.cha.base} modifier={this.state.cha.modifier}/>
         </div>
       </div>
   );}
--- src/StatBlock.js
+++ src/StatBlock.js
@@ -3,16 +3,6 @@ import './App.css';
 
 class StatBlock extends React.Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            name: this.props.name,
-            base: 10,
-            modifier: 0,
-        }
-    }
-
     calcModifier(stat) {
         return Math.floor((stat - 10) / 2);
     }
@@ -20,31 +10,22 @@ class StatBlock extends React.Component {
     updateStat = (event) => {
         var thisModifier = this.calcModifier(event.target.value);
 
-        this.setState({
-          base: event.target.value,
-          modifier: thisModifier
-        });
-
-        this.props.statchange(this.state.name);
+        this.props.statchange(this.props.name, event.target.value, thisModifier);
     }
 
     randomStat = (event) => {
-        var stat = Math.floor((Math.random()*15) + 3)
+        var stat = Math.floor((Math.random()*6) + (Math.random()*6) + (Math.random()*6))
         var thisModifier = this.calcModifier(stat);
 
-        this.setState({
-            base : stat,
-            modifier: thisModifier
-        })
+        this.props.statchange(this.props.name, stat, thisModifier);
     }
 
     render() {
         return(
         <div className="statbox">
-            <div className="statname">{this.state.name}:</div>
-            <div className="base"><textarea onChange={this.updateStat} value={this.state.base}/></div>
-            <div className="modifier">{this.state.modifier}</div>
-            <div className="randombutt" onClick={this.randomStat}></div>
+            <div className="statname" onClick={this.randomStat}>{this.props.name}:</div>
+            <div className="base"><textarea onChange={this.updateStat} value={this.props.base}/></div>
+            <div className="modifier">{this.props.modifier}</div>
         </div>
     );}
 }
